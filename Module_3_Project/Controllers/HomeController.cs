8bb7a670cad39c9d2fec1a5f18e9bff02f69441b
parodez <166378157+parodez@users.noreply.github.com>
2025-04-02 09:57:00 +0800

    added db login

--- Module_3_Project/Controllers/HomeController.cs
+++ Module_3_Project/Controllers/HomeController.cs
@@ -1,23 +1,50 @@
 using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Mvc;
 using Module_3_Project.Models;
+using MySql.Data.MySqlClient;
 
 namespace Module_3_Project.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private IConfiguration Configuration;
+        public HomeController(IConfiguration _configuration)
         {
-            _logger = logger;
+            Configuration = _configuration;
         }
 
-        public IActionResult Index()
+        public IActionResult AdminLogin(string txtUname, string txtPass)
         {
+            LoggedUser loggedUser = new LoggedUser();
+            loggedUser.LoggedIn = false;
+            loggedUser.UserName = "";
+
+            string sql = "SELECT * FROM usercredentials WHERE uname = '" + txtUname + "' AND upassword = '" + txtPass + "'";
+            string constr = this.Configuration.GetConnectionString("DefaultConnection");
+
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(sql, con))
+                {
+                    con.Open();
+
+                    using (MySqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        if (sdr.HasRows)
+                        {
+                            loggedUser.LoggedIn = true;
+                        }
+                    }
+                    con.Close();
+                }
+            }
+            if (loggedUser.LoggedIn == true)
+            {
+                return RedirectToAction("Privacy", "Home");
+            }
             return View();
         }
-
         public IActionResult Privacy()
         {
             return View();
---
+++ Module_3_Project/Models/LoggedUser.cs
@@ -0,0 +1,8 @@
+﻿namespace Module_3_Project.Models
+{
+    public class LoggedUser
+    {
+        public bool LoggedIn { get; set; }
+        public string UserName { get; set; }
+    }
+}

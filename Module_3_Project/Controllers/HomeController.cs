c03ad6bff2c820ba844b0bfe23cbd913fedddbae
Pigeonzz <jeromeelano@gmail.com>
2025-04-03 12:41:00 +0800

    Things that I changed/added:  - Added session to Program.cs  - Added session&cookies create and call to HomeController  - Added AI geenrated layout to _Layout.cshtml & site.css

--- Module_3_Project/Controllers/HomeController.cs
+++ Module_3_Project/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Mvc;
 using Module_3_Project.Models;
 using MySql.Data.MySqlClient;
+using Microsoft.AspNetCore.Http;
+using System;
 
 namespace Module_3_Project.Controllers
 {
@@ -16,6 +18,10 @@ namespace Module_3_Project.Controllers
         }
         public IActionResult Login(string txtUname, string txtPass, string btnLogin)
         {
+            // Getting cookie
+            string cookiename = HttpContext.Request.Cookies["MyCookie"];
+            Debug.WriteLine($"Cookie Value (in Login): {cookiename}");
+
             var loggedUser = new LoggedUser();
             if (btnLogin == "Admin")
             {
@@ -48,7 +54,18 @@ namespace Module_3_Project.Controllers
                 }
                 if (loggedUser.LoggedIn == true)
                 {
-                    return RedirectToAction("AdminView", "Home", new {term_id = "20241"});
+                    // Creation of cookie for Admin
+                    HttpContext.Response.Cookies.Append("MyCookie", "AdminCookie", new CookieOptions
+                    {
+                        Expires = DateTimeOffset.UtcNow.AddMinutes(1),
+                        HttpOnly = true,
+                        IsEssential = true
+                    });
+
+                    // Creation of session for Admin
+                    HttpContext.Session.SetString("MySession", "AdminLoggedIn");
+
+                    return RedirectToAction("AdminView", "Home", new { term_id = "20241" });
                 }
             }
             else if (btnLogin == "Student")
@@ -81,18 +98,46 @@ namespace Module_3_Project.Controllers
                 }
                 if (loggedUser.LoggedIn == true)
                 {
-                    return RedirectToAction("StudentView", "Home", new {stud_id = txtUname});
+                    // Creation of cookie for Student
+                    HttpContext.Response.Cookies.Append("MyCookie", "StudentCookie", new CookieOptions
+                    {
+                        Expires = DateTimeOffset.UtcNow.AddMinutes(1),
+                        HttpOnly = true,
+                        IsEssential = true
+                    });
+
+                    // Creation of session for Student
+                    HttpContext.Session.SetString("MySession", "StudentLoggedIn");
+
+                    return RedirectToAction("StudentView", "Home", new { stud_id = txtUname });
                 }
             }
-            
+
             return View();
         }
+
+
+
         public IActionResult AdminView(string term_id)
         {
+            // Getting session data and conditional check
+            string sessionname = HttpContext.Session.GetString("MySession");
+
+            if (sessionname == null)
+            {
+                Debug.WriteLine("No Session data");
+                return RedirectToAction("Login", "Home");
+            }
+            else
+            {
+                Debug.WriteLine(sessionname);
+               
+            }
+
             List<Terms> terms = new List<Terms>();
             List<Enrolled> enrolled = new List<Enrolled>();
             List<Student> students = new List<Student>();
-   
+
             string constr = this.Configuration.GetConnectionString("DefaultConnection");
             string sql;
 
@@ -187,6 +232,18 @@ namespace Module_3_Project.Controllers
         }
         public IActionResult AllStudentInfo()
         {
+            // Getting session data and conditional check (optional here, adjust as needed)
+            string sessionname = HttpContext.Session.GetString("MySession");
+            if (sessionname == null)
+            {
+                Debug.WriteLine("wala ng session data (AllStudentInfo)");
+                return RedirectToAction("MainPage", "Home");
+            }
+            else
+            {
+                Debug.WriteLine(sessionname + " (AllStudentInfo)");
+            }
+
             List<Student> students = new List<Student>();
 
             string constr = this.Configuration.GetConnectionString("DefaultConnection");
@@ -226,6 +283,18 @@ namespace Module_3_Project.Controllers
         }
         public IActionResult StudentView(string stud_id)
         {
+            // Getting session data and conditional check (optional here, adjust as needed)
+            string sessionname = HttpContext.Session.GetString("MySession");
+            if (sessionname == null)
+            {
+                Debug.WriteLine("wala ng session data (StudentView)");
+                return RedirectToAction("MainPage", "Home");
+            }
+            else
+            {
+                Debug.WriteLine(sessionname + " (StudentView)");
+            }
+
             Student student = new Student();
 
             string constr = this.Configuration.GetConnectionString("DefaultConnection");
@@ -260,10 +329,15 @@ namespace Module_3_Project.Controllers
 
             return View(student);
         }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult MainPage()
+        {
+            return View();
         }
     }
 }
